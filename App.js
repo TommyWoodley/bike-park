8f785d5f495efdbaedc1b067576752f67d0ada7c
Tom Woodley <thwoodley@gmail.com>
2022-06-08 16:32:04 +0100

    tw1820 - include google map api

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from ‘react-native-maps’;
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>You are cool</Text>
+      <MapView style={{height: '50%', width: '100%'}} provider={PROVIDER_GOOGLE} showsUserLocation={true} />
       <StatusBar style="auto" />
     </View>
   );
