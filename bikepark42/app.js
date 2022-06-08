b0c0fa94c90bdea0c2c5fc93304ce9b732a31fea
Norberto Mateos <nm920@ic.ac.uk>
2022-06-08 14:45:07 +0100

    change port

--- bikepark42/app.js
+++ bikepark42/app.js
@@ -17,7 +17,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
-app.listen(port);
+app.listen(process.env.PORT);
 
 app.use(logger('dev'));
 app.use(express.json());
