2ff81da14f9291417ff0929ddeaef18468a7c6c0
Norberto Mateos <nm920@ic.ac.uk>
2022-06-08 14:51:14 +0100

    change file name to not be repeated

--- bikepark42/downloads.js
+++ bikepark42/downloads.js
@@ -7,34 +7,34 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+var downloads = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+downloads.set('views', path.join(__dirname, 'views'));
+downloads.set('view engine', 'pug');
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
-app.listen(process.env.PORT);
+downloads.listen(process.env.PORT);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+downloads.use(logger('dev'));
+downloads.use(express.json());
+downloads.use(express.urlencoded({ extended: false }));
+downloads.use(cookieParser());
+downloads.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+downloads.use('/', indexRouter);
+downloads.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+downloads.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+downloads.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -44,4 +44,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = downloads;
