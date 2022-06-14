4f7fe6d303fe321a69a859e4d309c950dab7ab0b
Tom Woodley <thwoodley@gmail.com>
2022-06-14 15:17:18 +0100

    fix - map starts in correct position and moves when clicked on pin

--- src/scenes/mainMapView.js
+++ src/scenes/mainMapView.js
@@ -2,23 +2,36 @@ import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import {View} from 'react-native';
 import {firebase} from "../utils/config";
 import {onError, onResult} from "../utils/database";
-import {useEffect, useState} from "react";
+import {createRef, useEffect, useState} from "react";
 import * as Location from 'expo-location';
 import {ParkingMarker} from "../components/molecules";
 import InfoPopup from "../components/organisms/infoPopup";
 import {getDownloadURL, getStorage, ref} from "firebase/storage";
+import markerImage from "../components/atoms/markerImage";
 
 const fireRef = firebase.firestore().collection('locations');
+
+// function animateMap(lat, lng, mapView) {
+//     mapView.animateToRegion({ // Takes a region object as parameter
+//         longitude: lat,
+//         latitude: lng,
+//         latitudeDelta: 0.4,
+//         longitudeDelta: 0.4,
+//     },1000);
+// }
 
 export default function MainMapView() {
     const [fullScreen, setFullScreen] = useState(true);
     const [locations, setLocations] = useState([]);
-    const [currentLat, setCurrentLat] = useState(51.498317);
-    const [currentLong, setCurrentLong] = useState(-0.176923);
+    const [currentLat, setCurrentLat] = useState(0);
+    const [currentLong, setCurrentLong] = useState(0);
 
     const [selectedDesc, setSelectedDesc] = useState('');
 
     const [image, setImage] = useState('https://flevix.com/wp-content/uploads/2019/07/Untitled-2.gif');
+
+    const mapView = createRef();
+
 
     // Ask for location permission
     useEffect(() => {
@@ -30,6 +43,7 @@ export default function MainMapView() {
             let location = await Location.getCurrentPositionAsync({});
             setCurrentLat(location.coords.latitude);
             setCurrentLong(location.coords.longitude);
+            console.log("hello" + location.coords.latitude + location.coords.longitude);
         })();
     }, []);
 
@@ -47,7 +61,8 @@ export default function MainMapView() {
                 style={{height: fullScreen ? '100%' : '80%', width: '100%'}}
                 provider={PROVIDER_GOOGLE}
                 showsUserLocation={true}
-                initialRegion={{
+                ref={mapView}
+                region={{
                     latitude: currentLat,
                     longitude: currentLong,
                     latitudeDelta: 0.01,
@@ -60,6 +75,8 @@ export default function MainMapView() {
                         onClick={() => {
                             setFullScreen(() => false);
                             setSelectedDesc(marker.desc);
+                            setCurrentLat(marker.coord.latitude);
+                            setCurrentLong(marker.coord.longitude);
                             const storage = getStorage()
                             const reference = ref(storage, '/' + marker.img);
 
