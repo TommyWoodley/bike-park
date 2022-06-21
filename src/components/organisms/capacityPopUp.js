646243fb71bc4d66770d751e8a0624e894c5e24c
Tom Woodley <thwoodley@gmail.com>
2022-06-21 17:49:42 +0100

    give me a second I, I need to get my story straight...

---
+++ src/components/organisms/capacityPopUp.js
@@ -0,0 +1,107 @@
+import {Marker} from "react-native-maps";
+import {Image, Modal, Pressable, View, Text, StyleSheet} from "react-native";
+import {styles} from "./infoPopup";
+
+function CapacityPopUp({id, desc, image, visible, setVisible}) {
+    return (<Modal
+        animationType="slide"
+        transparent={true}
+        desc={desc}
+        image={image}
+        visible={visible}
+        onRequestClose={() => {
+            setVisible(false);
+        }}
+        style={{justifyContent: 'center', alignItems: 'center'}}
+    >
+        <View style={{justifyContent: 'center', alignContent: 'center'}}>
+            <View style={{
+                marginTop: '40%',
+                height: '50%',
+                width: '90%',
+                alignSelf:'center',
+                backgroundColor: "white",
+                borderRadius: 30,
+                padding: 10,
+                alignItems: "center",
+                elevation: 20
+            }}>
+                <View style={{flexDirection: 'row', height: '70%'}}>
+                    <Image source={{uri: image}}
+                           style={{
+                               height: '100%',
+                               width:'100%',
+                               flex: 3,
+                               borderRadius:20,
+                               overflow: 'hidden'}}/>
+                    <Text style={{fontSize: 18,
+                        fontWeight: '600',
+                        color: '#000000',
+                        padding: 10,
+                        flex: 2
+                    }}>How may bays are left in {desc}?</Text>
+
+                </View>
+                <View style={{flexDirection: 'row', paddingTop: 18}}>
+                    <Pressable
+                        style={{
+                            alignItems: 'center',
+                            width: '27%',
+                            alignContent: 'flex-start',
+                            paddingVertical: 10,
+                            marginLeft: 10,
+                            borderRadius: 20,
+                            backgroundColor: '#ffffff',
+                            borderWidth: 3,
+                            borderColor: '#000000',
+                        }}
+                        onPress={() => setVisible(false)}
+                    >
+                        <Text>All</Text>
+                    </Pressable>
+                    <Pressable
+                        style={{
+                            alignItems: 'center',
+                            width: '27%',
+                            alignContent: 'flex-start',
+                            paddingVertical: 10,
+                            marginLeft: 10,
+                            borderRadius: 20,
+                            backgroundColor: '#ffffff',
+                            borderWidth: 3,
+                            borderColor: '#000000',
+                        }}
+                        onPress={() => setVisible(false)}
+                    >
+                        <Text>Some</Text>
+                    </Pressable>
+                    <Pressable
+                        style={{
+                            alignItems: 'center',
+                            width: '27%',
+                            alignContent: 'flex-start',
+                            paddingVertical: 10,
+                            marginLeft: 10,
+                            borderRadius: 20,
+                            backgroundColor: '#ffffff',
+                            borderWidth: 3,
+                            borderColor: '#000000',
+                        }}
+                        onPress={() => setVisible(false)}
+                    >
+                        <Text>None</Text>
+                    </Pressable>
+                </View>
+            </View>
+        </View>
+
+    </Modal>);
+}
+
+
+
+export default CapacityPopUp;
+
+const modalStyles = StyleSheet.create({
+
+})
--- src/scenes/mainMapView.js
+++ src/scenes/mainMapView.js
@@ -8,11 +8,27 @@ import {ParkingMarker} from "../components/molecules";
 import InfoPopup from "../components/organisms/infoPopup";
 import {getDownloadURL, getStorage, ref} from "firebase/storage";
 import MapViewDirections from "react-native-maps-directions";
+import CapacityPopUp from "../components/organisms/capacityPopUp";
 
 const fireRef = firebase.firestore().collection('locations');
 
 function getLink(mLat, mLong) {
     return 'https://www.google.com/maps/dir/?api=1&destination=' + mLat + '%2C' + mLong + '&travelmode=bicycling'
+}
+
+function getDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371e3; // metr
+    const p1 = lat1 * Math.PI/180; // φ, λ in radians
+    const p2 = lat2 * Math.PI/180;
+    const dp = (lat2-lat1) * Math.PI/180;
+    const dl = (lon2-lon1) * Math.PI/180;
+
+    const a = Math.sin(dp/2) * Math.sin(dp/2) +
+        Math.cos(p1) * Math.cos(p2) *
+        Math.sin(dl/2) * Math.sin(dl/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    return R * c;
 }
 
 export default function MainMapView() {
@@ -28,10 +44,17 @@ export default function MainMapView() {
     const [duration, setDuration] = useState(0);
 
     const [selectedDesc, setSelectedDesc] = useState('');
+
     const [selectedNumStars, setSelectedNumStars] = useState(0);
     const [selectedNumReviews, setSelectedNumReviews] = useState(0);
     const [selectedCapacity, setSelectedCapacity] = useState(0);
     const [selectedShelter, setSelectedShelter] = useState(false);
+
+    const [closeVisible, setCloseVisible] = useState(false);
+    const [closest, setClosest] = useState({
+        desc: '',
+    });
+    const [closestImg, setClosestImg] = useState('');
 
     const [image, setImage] = useState('https://flevix.com/wp-content/uploads/2019/07/Untitled-2.gif');
 
@@ -90,8 +113,43 @@ export default function MainMapView() {
             let location = await Location.getCurrentPositionAsync({});
             setCurrentLat(location.coords.latitude);
             setCurrentLong(location.coords.longitude);
+            setGeoLat(location.coords.latitude);
+            setGeoLong(location.coords.longitude);
+
         })();
     }, []);
+
+    useEffect(() => {
+        (async () => {
+            let location = await Location.getCurrentPositionAsync({});
+            const ps = locations.map((marker, index) => (
+                {
+                    index: index,
+                    distance: getDistance(marker.coord.latitude, marker.coord.longitude, location.coords.latitude, location.coords.longitude),
+                    marker: marker
+                }
+            )).filter((obj, index) => obj.distance < 5).sort((a, b) => a.distance > b.distance);
+
+            if (ps.length > 0) { // the closest one within some metres
+                const mark = ps.at(0).marker;
+                console.log(mark);
+                setClosest(ps.at(0).marker);
+                setClosestImg('https://flevix.com/wp-content/uploads/2019/07/Untitled-2.gif');
+                setCloseVisible(true);
+                const storage = getStorage();
+                const reference = ref(storage, '/' + mark.img);
+
+                getDownloadURL(reference)
+                    .then((x) => {
+                        setClosestImg(x);
+                    })
+                    .catch(e => {
+                        console.log(mark.desc + 'getting downloadURL of image error =>  ' + '/' + mark.img, e);
+                        setImage('https://storcpdkenticomedia.blob.core.windows.net/media/recipemanagementsystem/media/recipe-media-files/recipes/retail/desktopimages/rainbow-cake600x600_2.jpg?ext=.jpg');
+                    })
+            }
+        })();
+    }, [geoLat, geoLong, locations]);
 
     useEffect(() => {
         fireRef
@@ -101,6 +159,12 @@ export default function MainMapView() {
 
     return (
         <View style={{flex: 1}}>
+            <CapacityPopUp
+                visible={closeVisible}
+                setVisible={setCloseVisible}
+                desc={closest.desc}
+                image={closestImg}
+            />
             <InfoPopup
                 style={{height: fullScreen === 'full' ? '40%' : '25%', width: '100%'}}
                 lat={currentLat}
